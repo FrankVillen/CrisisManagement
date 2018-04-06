dc95ae1037a42ce19d6ed7c5b4d62d1383f1dda9
Villen <33656418+FrankVillen@users.noreply.github.com>
2018-04-06 17:55:12 +0200

    Add files via upload

---
+++ Code/DataConfig.py
@@ -0,0 +1,4 @@
+consumer_key = 'WT0opCGs2pJPBWvi2FWYfiEcP'
+consumer_secret = 'hkQldzepXZkRIEJ1OwJ7SVkUnv9Tx6AeBXT1kXIHAZKyIiCjrV'
+access_token = '934951411376091127-Vn62rFoVgQQpFxZqmjxqO7DqCv3kUld'
+access_token_secret = 'ho1tY9WnxDvW0JA4eTc5r4HlUsPZITQsFha6021stSzLr'
