34eea1211c13e5b539ad4bd9ce97d16ba0fda8ea
Villen <33656418+FrankVillen@users.noreply.github.com>
2018-04-06 17:44:50 +0200

    Delete DataConfig.py

--- Code/DataConfig.py
+++
@@ -1,4 +0,0 @@
-consumer_key = 'WT2opCGs3pJPVWvi2FWYfoEcP'
-consumer_secret = 'hkQldzepXZkRIEJ1OwJ7wVkUnv3Tx6AiBUT4kXIHAZKyIiCgrV'
-access_token = '913641411376091137-Vn62rFoVgQQpFxZqmjxqO7CqCv3kUlt'
-access_token_secret = 'ho1kY8WnxDvW0JA4eTd5r4HlUsPLITQsFja6053sySzLr'
